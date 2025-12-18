fc95c0451a11ea728d95bebcdb9487f621e54c7e
sathwik-1804 <valgundawarsunnysathwik@gmail.com>
2025-12-18 19:19:47 +0530

    Added Chatbot_Music_System project

---
+++ Chatbot_Music_System/MusicApp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="home"),   # 👈 add this line
+    path("index.html", views.index, name="index"),
+    path("AdminLogin.html", views.AdminLogin, name="AdminLogin"),
+    path("AdminLoginAction", views.AdminLoginAction, name="AdminLoginAction"),
+    path("UserLogin.html", views.UserLogin, name="UserLogin"),
+    path("UserLoginAction", views.UserLoginAction, name="UserLoginAction"),
+    path("RegisterAction", views.RegisterAction, name="RegisterAction"),
+    path("Register.html", views.Register, name="Register"),
+    path("UploadMusic.html", views.UploadMusic, name="UploadMusic"),
+    path("UploadMusicAction", views.UploadMusicAction, name="UploadMusicAction"),
+    path("ViewUser", views.ViewUser, name="ViewUser"),
+    path("Chatbot.html", views.Chatbot, name="Chatbot"),
+    path("ChatData", views.ChatData, name="ChatData"),
+    path("MusicPlay", views.MusicPlay, name="MusicPlay"),
+    path("Stop", views.Stop, name="Stop"),
+]
